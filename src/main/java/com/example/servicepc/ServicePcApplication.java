9db78ef50e8d0bf97e55963262736d91d10b011f
Pop Mircea-Stefan <pop_mirceastefan@yahoo.ro>
2024-09-13 10:12:26 +0300

    backend push

---
+++ src/main/java/com/example/servicepc/ServicePcApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicepc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicePcApplication.class, args);
+    }
+
+}
